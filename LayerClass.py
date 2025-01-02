3113fd51c49726e3567b2424e5295caa06954528
AlbertoMontanelli <albertomontanelli@live.it>
2025-01-02 19:11:41 +0100

    Correzione di errori di sintassi quali dim_layer, dim_prev_layer, output_size

--- LayerClass.py
+++ LayerClass.py
@@ -1,4 +1,5 @@
 import numpy as np
+from RegularizationOptimizationClass import Regularization, Optimization
 
 class Layer:
 
@@ -24,15 +25,15 @@ class Layer:
         self.dim_layer = dim_layer
         self.activation_function = activation_function
         self.d_activation_function = d_activation_function
-        self.initialize_parameters()
+        self.initialize_weights_biases()
 
-    def initialize_parameters(self):
+    def initialize_weights_biases(self):
         '''
         Function that initializes the Weights and the Biases of the network
         '''
-        self.weights = np.random.uniform(low=-1/np.sqrt(self.dim_layer), high=1/np.sqrt(self.dim_prev_layer), 
+        self.weights = np.random.uniform(low=-1/np.sqrt(self.dim_prev_layer), high=1/np.sqrt(self.dim_prev_layer), 
                                          size=(self.dim_prev_layer, self.dim_layer))
-        self.biases = np.zeros((1, self.output_size))
+        self.biases = np.zeros((1, self.dim_layer))
 
     def forward_layer(self, input):
         '''
@@ -49,3 +50,19 @@ class Layer:
         self.net = np.dot(self.input, self.weights) + self.biases
         output = self.activation_function(self.net)
         return output
+    
+    def backward_layer(self, loss_gradient, optimizer):
+        '''
+        Function that computes the gradient loss and updates the weights and biases by the learning rule for the single layer
+
+        Args:
+            loss_gradient (array): gradient loss with respect to the layer output.
+            regularizer (Regularization): istance of Regularization class.
+            optimizer (Optimization): istance of Optimization class.
+
+        Returns:
+            sum_delta_weights (array): loss_gradient for hidden layer
+        '''
+        sum_delta_weights = optimizer.optimization(self.input, loss_gradient, self.d_activation_function)
+
+        return sum_delta_weights
