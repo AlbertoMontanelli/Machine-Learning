37ccc5161eb9881ea683fd456a0f37edd0896842
irene <i.bini3@studenti.unipi.it>
2025-01-14 13:07:53 +0100

    unit test per CUP

--- CUPDataProcessing.py
+++ CUPDataProcessing.py
@@ -1,13 +1,32 @@
 import pandas as pd
+import numpy as np
 
-file_path = "monk+s+problems/ML-CUP24-TR.csv"
+from DataProcessingClass import DataProcessing
+
+file_path = "ML-CUP24-TR.csv"
 
 skip_lines = 7 # skipping metadata
 
 first_column = ['Id']
 last_columns = ['target_x', 'target_y', 'target_z']
-middle_columns = [f'input_{i}' for i in range(1, 14)]
+middle_columns = [f'input_{i}' for i in range(1, 13)]
 
 columns = first_column + middle_columns + last_columns
-print(columns)
 df = pd.read_csv(file_path, sep = ",", skiprows = skip_lines, names = columns, header = None)
+#print(f"df: \n {df.iloc[0]}")
+
+data = df.drop(columns = ['Id', 'target_x', 'target_y', 'target_z'])
+print(f"data: \n {data}")
+target = df.drop(columns = [f'input_{i}' for i in range(1, 13)])
+target = target.drop(columns = ['Id'])
+print(f"target: \n {target}")
+
+
+data = data.to_numpy()
+target = target.to_numpy()
+
+print(f"data: \n {data}")
+print(f"target: \n {target}")
+
+CUP_data_splitter = DataProcessing(data, target, 0.2, K=6)
+
--- UnitTest.py
+++ UnitTest.py
@@ -1,23 +1,24 @@
 import numpy as np
 
-from DataProcessingClass import DataProcessing
+#from DataProcessingClass import DataProcessing
 from NeuralNetworkClass import NeuralNetwork
 from Functions import activation_functions, d_activation_functions, loss_functions, d_loss_functions
 from TrainingValidationClass import TrainingValidation
+from CUPDataProcessing import CUP_data_splitter
 
 '''Unit test for NN'''
-np.random.seed(42)
+np.random.seed(12)
 
 # Configurazione dei layer: [(input_dim, output_dim, activation_function, d_activation_function), ...]
 layers_config = [
-    (15, 32, activation_functions['ELU'], d_activation_functions['d_ELU']),
+    (12, 32, activation_functions['ELU'], d_activation_functions['d_ELU']),
     (32, 16, activation_functions['ELU'], d_activation_functions['d_ELU']),
     (16, 3, activation_functions['linear'], d_activation_functions['d_linear'])
 ]
 
 # Configurazione della regolarizzazione
 reg_config = {
-    'Lambda': 0.0001,
+    'Lambda': 1e-3,
     'alpha' : 0.5,
     'reg_type': 'elastic'
 }
@@ -25,7 +26,7 @@ reg_config = {
 # Configurazione dell'ottimizzazione
 opt_config = {
     'opt_type': 'NAG',
-    'learning_rate': 0.00001,
+    'learning_rate': 1e-6,
     'momentum': 0.9,
     'beta_1': 0.9,
     'beta_2': 0.999,
@@ -34,6 +35,27 @@ opt_config = {
 
 nn = NeuralNetwork(layers_config, reg_config, opt_config)
 
+epochs = 500
+batch_size = 20
+
+train_val = TrainingValidation(CUP_data_splitter, epochs, batch_size, loss_functions['mse'], d_loss_functions['d_mse'], nn)
+train_error_tot, val_error_tot = train_val.train_fold()
+
+# Plot degli errori
+import matplotlib.pyplot as plt
+
+plt.plot(train_error_tot, label='Training Error')
+plt.plot(val_error_tot, label='Validation Error')
+plt.xlabel('Epochs')
+plt.ylabel('Error')
+plt.yscale('log')
+plt.legend()
+plt.savefig('FigureCUP')
+plt.show()
+
+
+'''
+#unit test random 
 x_tot = np.random.rand(255, 15)
 target_tot = np.random.rand(255, 3)
 
@@ -46,8 +68,6 @@ batch_size = 30
 train_val = TrainingValidation(data_split, epochs, batch_size, loss_functions['mse'], d_loss_functions['d_mse'], nn)
 train_error_tot, val_error_tot = train_val.train_fold()
 
-
-
 # Plot degli errori
 import matplotlib.pyplot as plt
 
@@ -58,5 +78,4 @@ plt.ylabel('Error')
 plt.yscale('log')
 plt.legend()
 plt.show()
-
-
+'''
