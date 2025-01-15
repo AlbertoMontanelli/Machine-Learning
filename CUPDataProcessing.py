59cf6c3070a4dbfa4d4ed8843f2a28321d979765
leobm <147031896+leonardobandiera@users.noreply.github.com>
2025-01-15 20:14:39 +0100

    aggiunto dataset cup test + CUPDataProcessing con test set cup

--- CUPDataProcessing.py
+++ CUPDataProcessing.py
@@ -3,30 +3,35 @@ import numpy as np
 
 from DataProcessingClass import DataProcessing
 
-file_path = "ML-CUP24-TR.csv"
+# Importing labelled data
+file_path_labelled = "ML-CUP24-TR.csv"
 
 skip_lines = 7 # skipping metadata
 
-first_column = ['Id']
-last_columns = ['target_x', 'target_y', 'target_z']
-middle_columns = [f'input_{i}' for i in range(1, 13)]
+id_column = ['Id']
+target_columns = ['target_x', 'target_y', 'target_z']
+input_columns = [f'input_{i}' for i in range(1, 13)]
+columns = id_column + input_columns + target_columns
 
-columns = first_column + middle_columns + last_columns
-df = pd.read_csv(file_path, sep = ",", skiprows = skip_lines, names = columns, header = None)
-#print(f"df: \n {df.iloc[0]}")
-
+df = pd.read_csv(file_path_labelled, sep = ",", skiprows = skip_lines, names = columns, header = None)
 data = df.drop(columns = ['Id', 'target_x', 'target_y', 'target_z'])
-print(f"data: \n {data}")
-target = df.drop(columns = [f'input_{i}' for i in range(1, 13)])
-target = target.drop(columns = ['Id'])
-print(f"target: \n {target}")
+target = df.drop(columns = ['Id'] + [f'input_{i}' for i in range(1, 13)])
 
-
+# Conversion to numpy arrays
 data = data.to_numpy()
 target = target.to_numpy()
 
-print(f"data: \n {data}")
-print(f"target: \n {target}")
+# Splitting the CUP labelled data into training set, validation set and test set.
+# A K-Fold Cross Validation with K = 6 will be performed during model selection.
+CUP_data_splitter = DataProcessing(data, target, test_perc = 0.2, K = 6)
 
-CUP_data_splitter = DataProcessing(data, target, 0.2, K=6)
+# Importing blind data
+file_path_blind = "ML-CUP24-TS.csv"
 
+columns_blind = id_column + input_columns
+
+df_blind = pd.read_csv(file_path_blind, sep = ",", skiprows = skip_lines, names = columns_blind, header = None)
+data_blind = df_blind.drop(columns = ['Id'])
+
+# Conversion to numpy arrays
+data_blind = data_blind.to_numpy()
