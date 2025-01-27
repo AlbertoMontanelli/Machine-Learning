ec5e914b8b1d99abf1b4eaa8efebe5c94b4214c4
AlbertoMontanelli <albertomontanelli@live.it>
2025-01-27 12:07:26 +0100

    codice e grafici per Monk3

--- Monk3.py
+++ Monk3.py
@@ -39,14 +39,14 @@ reg_config = {
 # Optimization configuration
 opt_config = {
     'opt_type': 'none',
-    'learning_rate': 5e-3,
+    'learning_rate': 0.01,
     'momentum': 0.9,
     'beta_1': 0.9,
     'beta_2': 0.999,
     'epsilon': 1e-8,
 }
 
-epochs = 1000
+epochs = 500
 batch_size = 1
 
 
@@ -83,27 +83,25 @@ train_error, test_error, accuracy_train, accuracy_test = train_test.retrain_test
 ########################################################################################################################
 
 import matplotlib.pyplot as plt
-from matplotlib.ticker import LogLocator, LogFormatterMathtext
+from matplotlib.ticker import LogLocator, ScalarFormatter
 
 # Figure
-fig, ax = plt.subplots(1, 2, figsize = (15, 10))
+plt.figure()
 
 # Loss functions graph
-line_train, = ax[0].plot(train_error, label = 'Training Error', linewidth = 2)
-line_test, = ax[0].plot(test_error, label = 'Test Error', linewidth = 2)
-ax[0].set_xlabel('Epochs', fontsize = 16, fontweight = 'bold')
-ax[0].set_ylabel('Error', fontsize = 16, fontweight = 'bold')
-ax[0].set_yscale('log')
-ax[0].yaxis.set_major_locator(LogLocator(base=10.0, subs=np.arange(1.0, 10.0) * 0.1, numticks=10))
-ax[0].yaxis.set_major_formatter(LogFormatterMathtext(base=10)) # Configure the y-axis to display labels in scientific notation.
-ax[0].grid()
+plt.plot(train_error, label = 'Training Error', linewidth = 2)
+plt.plot(test_error, label = 'Test Error', linewidth = 2)
+plt.xlabel('Epochs', fontsize = 18, fontweight = 'bold')
+plt.ylabel('Error', fontsize = 18, fontweight = 'bold')
+plt.yscale('log')
 
-# Accuracy graph
-line_train_acc, = ax[1].plot(accuracy_train, label = 'Training accuracy', linewidth = 2)
-line_test_acc, = ax[1].plot(accuracy_test, label = 'Test accuracy', linewidth = 2)
-ax[1].set_xlabel('Epochs', fontsize = 16, fontweight = 'bold')
-ax[1].set_ylabel('Accuracy', fontsize = 16, fontweight = 'bold')
-ax[1].grid()
+ax = plt.gca()  # Ottieni gli assi correnti
+ax.yaxis.set_major_locator(LogLocator(base=10.0, subs=np.arange(1.0, 10.0) * 0.1, numticks=10))
+ax.yaxis.set_major_formatter(ScalarFormatter())
+
+plt.grid()
+
+
 
 # Creation of a legend containing the details of the neural network
 network_details = [
@@ -115,36 +113,69 @@ network_details = [
     ('Regularization', f"{reg_config['reg_type']}"),
     ('lambda', f"{reg_config['Lambda']}"),
     ('alpha', f"{reg_config['alpha']}"),
-    ('Optimizer',f"{opt_config['opt_type']}")
+    ('Optimizer',f"{opt_config['opt_type']}"),
+    ('Batch size', f"{batch_size}")
 ]
 
 legend_info = "\n".join([f"{param}: {value}" for param, value in network_details])
 
-ax[0].legend(handles = [line_train, line_test], labels = ['Training Error', 'Test Error'], fontsize = 18)
-ax[1].legend(handles = [line_train_acc, line_test_acc], labels = ['Training Accuracy', 'Test Accuracy'], 
-             fontsize = 18, loc = 'best')
+plt.legend(labels = ['Training Error', 'Test Error'], fontsize = 25, loc = 'lower left')
 
-ax[1].text(0.72, 0.305, "Network characteristics", transform = ax[1].transAxes, fontsize = 16, 
+
+plt.text(0.86, 0.965, "Network characteristics", transform = ax.transAxes, fontsize = 16, 
         fontweight = 'bold', horizontalalignment = 'center', verticalalignment = 'top')
 
 props = dict(boxstyle = 'round', facecolor = 'white', edgecolor = 'black')
-ax[1].text(0.72, 0.27, legend_info, transform = ax[1].transAxes, fontsize = 16, 
+plt.text(0.86, 0.930, legend_info, transform = ax.transAxes, fontsize = 16, 
         verticalalignment = 'top', horizontalalignment = 'center', bbox = props)
 
-ax[0].set_title('Error vs Epochs', fontsize = 18, fontweight = 'bold')
-ax[1].set_title('Accuracy vs Epochs', fontsize = 18, fontweight = 'bold')
+plt.title('Error vs Epochs', fontsize = 20, fontweight = 'bold')
+
 
 plt.tight_layout()
 
-for a in ax:
-    a.tick_params(axis = 'x', labelsize = 16)
-    a.tick_params(axis = 'y', labelsize = 16)
+plt.tick_params(axis = 'x', labelsize = 16)
+plt.tick_params(axis = 'y', labelsize = 16)
+    
 
 manager = plt.get_current_fig_manager()
 manager.full_screen_toggle() 
 
 plt.pause(2)
 
-plt.savefig(f"grafici/monk3_batch{batch_size}_learningrate{opt_config['learning_rate']}_reg_{reg_config['reg_type']}_lambda{reg_config['Lambda']}_opt_{opt_config['opt_type']}.pdf", bbox_inches = 'tight', dpi = 1200)
+plt.savefig("grafici_per_slides/Monk3_error_reg.pdf", bbox_inches = 'tight', dpi = 1200)
 
+
+plt.close()
+plt.show()
+
+
+# Accuracy graph
+
+plt.figure()
+plt.plot(accuracy_train, label = 'Training accuracy', linewidth = 2)
+plt.plot(accuracy_test, label = 'Test accuracy', linewidth = 2)
+plt.xlabel('Epochs', fontsize = 18, fontweight = 'bold')
+plt.ylabel('Accuracy', fontsize = 18, fontweight = 'bold')
+plt.grid()
+
+
+plt.legend(labels = ['Training Accuracy', 'Test Accuracy'], fontsize = 25, loc = 'best')
+
+plt.title('Accuracy vs Epochs', fontsize = 20, fontweight = 'bold')
+
+plt.tight_layout()
+
+plt.tick_params(axis = 'x', labelsize = 16)
+plt.tick_params(axis = 'y', labelsize = 16)
+
+manager = plt.get_current_fig_manager()
+manager.full_screen_toggle() 
+
+plt.pause(2)
+
+plt.savefig("grafici_per_slides/Monk3_accuracy_reg.pdf", bbox_inches = 'tight', dpi = 1200)
+
+
+plt.close()
 plt.show()
